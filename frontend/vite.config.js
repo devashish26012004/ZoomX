f8eeff8c94adbc3209031d80a9b476c0d1b3028b
Devashish Dumbhare <devashishimp26@gmail.com>
2025-09-05 17:41:57 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ✅ ensures routes work on refresh in production
+  base: "/", // critical for production routing
+  build: {
+    outDir: "dist",
+  },
 });
